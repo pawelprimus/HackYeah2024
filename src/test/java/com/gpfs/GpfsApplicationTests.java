162e0d73856cacb60b6ea32dd1a65cd7fa3f9b06
m4gik <michal@kams.com.pl>
2024-09-28 18:08:40 +0200

    Init repo for Hackathon contest

---
+++ src/test/java/com/gpfs/GpfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
